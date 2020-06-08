555d00107fe6d238f985d0cb7b75408d01f7b4ce
JayPhil2k19 <57634441+JayPhil2k19@users.noreply.github.com>
2020-06-07 19:32:06 -0600

    Add files via upload

--- config.h
+++ config.h
@@ -2,25 +2,31 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 600;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Mononoki Nerd Font:size=16"
+	"Hack-bold:size=16:style=bold",
+	"JoyPixels:pixelsize=8:antialias=true:autohint=true"
 };
+static const unsigned int bgalpha = 0xe0;
+static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbc5ff", "#282a36"}, //282a36
-	[SchemeSel] = { "#282a36", "#82aaff" },
-	[SchemeSelHighlight] = { "#82aaff", "#000000" },
-	[SchemeNormHighlight] = { "#f1fa8c", "#000000" },
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
-	[SchemeMid] = { "#eeeeee", "#770000" },
 };
+static const unsigned int alphas[SchemeLast][2] = {
+	/*		fgalpha		bgalphga	*/
+	[SchemeNorm] = { fgalpha, bgalpha },
+	[SchemeSel] = { fgalpha, bgalpha },
+	[SchemeOut] = { fgalpha, bgalpha },
+};
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 22;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
@@ -29,4 +35,4 @@ static unsigned int lineheight = 22;         /* -h option; minimum height of a m
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 3;
+static const unsigned int border_width = 5;
